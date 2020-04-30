0c95c154ece27d5d16ed0424e67ed4fbf008bd15
Louzada <silvalouzada@gmail.com>
2020-04-29 23:49:46 -0300

    Solved challenge

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,25 +1,39 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
-transactionRouter.get('/', (request, response) => {
+transactionRouter.get('/', (req, res) => {
   try {
-    // TODO
+    return res.json({
+      transactions: transactionsRepository.all(),
+      balance: transactionsRepository.getBalance(),
+    });
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    return res.status(400).json({ error: err.message });
   }
 });
 
-transactionRouter.post('/', (request, response) => {
+transactionRouter.post('/', (req, res) => {
+  const { title, value, type } = req.body;
   try {
-    // TODO
+    const createTransactionService = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransactionService.execute({
+      title,
+      value,
+      type,
+    });
+
+    return res.json(transaction);
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    return res.status(400).json({ error: err.message });
   }
 });
 
